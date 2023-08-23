731bfa7064ef4514bf0f0b615babefba3c639973
LanaKulakova <lanakulakova@gmail.com>
2023-08-23 22:29:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import {data} from "./data";
 import './App.css';
 
 function App() {
+const [rest, setRest] = useState(0);
+const {id, name, address, description, image} = data[rest];
+
+
+const previousRest = ()=> {
+setRest((rest => {
+  rest --;
+  if (rest < 0 ) {
+    return data.length-1;
+  }
+  return rest;
+}))
+}
+
+const nextRest = ()=> {
+  setRest((rest => {
+    rest ++;
+        if (rest > data.length-1) {
+      rest = 0;
+    }
+    return rest;
+  }))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+<div className="container">
+  <h1> TOP-5 Restaurants to visit in Paris </h1>
+</div>
+      
+
+<div className="container">
+  <h3> {id} - {name} </h3>
+</div>
+
+<div className="container">
+<img src={image} width="500px" alt="restaurant"  />
+</div>
+
+<div className="container">
+  <h3> {description} </h3>
+</div>
+
+<div className="container">
+  <h4> {address} </h4>
+</div>
+
+<div className="btn container">
+  <button onClick={previousRest}> Previous </button>
+  <button onClick={nextRest}> Next </button>
+</div>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,49 @@
+import gigi from "./gigi.jpg";
+import lesdeuxmargots from "./lesdeuxmargots.jpg";
+import lesfoodies from "./lesfoodies.jpg";
+import letrainbleu from "./letrainbleu.jpg";
+import perruche from "./perruche.jpg";
+
+export const data = [
+{
+    id: 1,
+    name: "Gigi Paris",
+    image: gigi,
+    address: "15 Av. Montaigne, 75008 Paris, France",
+    description: "Fancy Italian cuisine in a warm, refined space with views of Parisian rooftops & the Eiffel Tower."
+},
+
+{
+    id: 2,
+    name: "Les Deux Margots",
+    image: lesdeuxmargots,
+    address: "6 Pl. Saint-Germain des Prés, 75006 Paris, France",
+    description: "Opened in 1885, this iconic brasserie serves traditional French fare in an airy, charming space."
+},
+
+{
+    id: 3,
+    name: "Les Foodies",
+    image: lesfoodies,
+    address: "6-8 Sq. Sainte-Croix de la Bretonnerie, 75004 Paris, France",
+    description: "Asian-influenced Mediterranean food, plus set menus, at a trendy venue with velvet couches."
+},
+
+{
+    id: 4,
+    name: "Le Train Bleu",
+    image: letrainbleu,
+    address: "Paris Gare de Lyon (Doublon), Pl. Louis-Armand hall 1, 75012 Paris, France",
+    description: "Classic French dishes like foie gras & steak tartare in a lavish space with frescoes & chandeliers."
+},
+
+{
+    id: 5,
+    name: "Perruche",
+    image: perruche,
+    address: "Printemps De L'Homme, 2 Rue du Havre 9ème étage, 75009 Paris, France",
+    description: "Rooftop restaurant in the Printemps Haussmann department store serving classic Provencal cuisine."
+}
+
+
+]
